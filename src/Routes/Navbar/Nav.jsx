edecded972cbe593d8beddf44e64319d3d6da87b
Gele <gellehussen@gmail.com>
2023-11-18 21:09:33 +0100

    project initilization

---
+++ src/Routes/Navbar/Nav.jsx
@@ -0,0 +1,25 @@
+import MassageIcon from "../../Assets/AFROICOVN.png"
+import Link from "react"
+const Navbar = ()=>{
+
+
+
+
+    return(
+        <div className="navContainer">
+            <div className="navLogo">
+                <img src={MassageIcon} />
+            </div>
+
+            <div className="navLinks">
+
+            </div>
+        </div>
+    )
+
+
+}
+
+
+
+export default Navbar;
