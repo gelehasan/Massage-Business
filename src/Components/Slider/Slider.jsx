a9e4063753b503f7cb3e570ffec7ac8fdd40d8aa
Gele <gellehussen@gmail.com>
2023-11-19 21:43:31 +0100

    created slider use case

---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const Slider = ()=>{
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slides = [
+        { content: <div>Slide 1</div> },
+        { content: <div>Slide 2</div> },
+        { content: <div>Slide 3</div> },
+      ];
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Slider;
