ef4afb7a9c7adf201ab81be5489d8b5b6ef56b42
Gele <gellehussen@gmail.com>
2023-11-19 22:46:28 +0100

    finilized slider show

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,50 +1,64 @@
-import { useState, useEffect } from "react";
-import "./sliderStyle.css"
-const Slider = ()=>{
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const slides = [
-        { content: <div>Slide 1</div> },
-        { content: <div>Slide 2</div> },
-        { content: <div>Slide 3</div> },
-      ];
+import React, { useState, useEffect } from "react";
+import "./sliderStyle.css";
 
+const slides = [
+  {
+    h1Text: "Massage and Fun",
+    pText: "Okay just have fun",
+    imgSrc: "noWay"
+  },
+  {
+    h1Text: "Massage and boring",
+    pText: "Okay just have boring",
+    imgSrc: "noWay"
+  },
+];
+
+const Slider = () => {
+  const [count, setCount] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(slides[count]);
+
+  const { h1Text, pText } = currentSlide;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+      setCount((prevCount) => (prevCount + 1) % slides.length);
+      setCurrentSlide(slides[count]); // Issue: should be setCount((prevCount) => (prevCount + 1) % slides.length);
     }, 3000);
 
     return () => clearInterval(interval);
-  }, [slides.length]);
+  }, [count]);
 
   const handleCircleClick = (index) => {
-    setCurrentSlide(index);
+    setCount(index);
+    setCurrentSlide(slides[index]);
   };
 
-    return (
-
-       
-        <div className="slider-container">
-    <img className="sliderImg" src="https://images.unsplash.com/photo-1617952986600-802f965dcdbc?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+  return (
+    <div className="slider-container">
+      <img
+        className="sliderImg"
+        src="https://images.unsplash.com/photo-1617952986600-802f965dcdbc?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Slider Background"
+      />
       <div className="slides">
-        <h1> Message & Comestic</h1>
-        <p>Uppstäck vårt djupa engagemang för att förena kropp, själ och sinne gemom afrikansk massage och filosofin av Ubuntu </p>
+        <h1>{h1Text}</h1>
+        <p>{pText}</p>
         <button>Boka Nu</button>
       </div>
-      {/*<div className="circles">
+      {/* Circles section (uncomment if needed)
+      <div className="circles">
         {slides.map((_, index) => (
           <div
             key={index}
-            className={`circle ${index === currentSlide ? 'active' : ''}`}
+            className={`circle ${index === count ? 'active' : ''}`}
             onClick={() => handleCircleClick(index)}
           />
         ))}
-      </div>*/
-}
+      </div>
+      */}
     </div>
-
-    )
-}
-
+  );
+};
 
 export default Slider;
