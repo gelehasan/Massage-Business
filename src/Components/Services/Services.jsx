128b5b72ea0931c39b4abde8cda819d7f4371658
Gele <gellehussen@gmail.com>
2023-11-20 22:10:03 +0100

    added texts for the service description

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -8,6 +8,14 @@ const Services = ()=>{
         <div className="serviceContainer">
             <div className="serviceHeader">
                 <img src={serviceBackGround}/>
+
+                <div className="details">
+                    <h3>AFRIKANSK SIGNATUR</h3>
+                    <p>Vårt uppdrag är att vägleda dig på denna resa av självförverkligande, hjälpa dig att bryta dig fri från begränsningar och ge dig möjlighet att omfamna ditt sanna jag</p>
+
+                    <span>Home</span> <span>»</span> <span>Tjänster</span> <span>»</span> <span>Afrikansk Signatur</span>
+
+                </div>
             </div>
             
 
