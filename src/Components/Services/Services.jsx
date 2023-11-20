d251f8829bcfe37532d26fdcaf70bd16b6c7f0ee
Gele <gellehussen@gmail.com>
2023-11-20 22:03:46 +0100

    styled top service header

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import serviceBackGround from "../../Assets/massageimg.png";
+import "./serviceStyle.css";
 
 
 const Services = ()=>{
@@ -6,10 +7,10 @@ const Services = ()=>{
     return(
         <div className="serviceContainer">
             <div className="serviceHeader">
-
+                <img src={serviceBackGround}/>
             </div>
             
-            
+
         </div>
     )
 }
