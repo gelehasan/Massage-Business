374deb4ebc9ed6394668b1bdcccdc8d3e809c9ed
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-21 12:44:48 +0100

    Included more content in the service page

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -17,18 +17,50 @@ const Services = ()=>{
 
                 </div>
             </div>
-
-
-                <div class="serviceContent">
+/*Dont touch*/
+ <div class="serviceContent">
       
       <div class="contetLeft">
+        <h2> Vad är AFROROOTS SPECIAL? </h2>
         
+        <p>
+          Förbättrar blodcirkulationen, minskar högt blodtryck och hjälper till med sömnstörningar. Massagen består av flödande slag över hela kroppens längd, passiva ledrörelser och djupt strukturellt arbete på muskler och leder. 
+ </p>
+        <p>
+Genom medveten beröring skärps den sensoriska uppfattningen av kroppen och en harmonisk balans skapas. På detta sätt kan fysiska eller energiska blockeringar av klienterna släppas. 
+        </p>
+        
+        
+        <h2>
+          Hur verkar AFROROOTS SPECIAL? 
+        </h2>
+        
+        <p>
+          AfroRoots Signaturmassage erbjuder den ultimata   upplevelsen för att höja humöret och bekämpa jetlag, dålig cirkulation, utbrändhet och trötthet. 
+        </p>
+        
+        <ul>
+          
+          <li> Särskilt utformad för resenärer, är detta en perfekt kombination av östliga och västerländska tekniker som maximerar din komfortnivå. </li>
+          
+          <li> Genom användning av stretching och palming-tekniker lyfts och återupplivas kroppen</li>
+          <li>
+            Vi använder noggrant utvalda ekologiska oljor från Afrika som ger antingen en lugnande eller föryngrande effekt. Denna unika blandning tillför fördelarna med djupvävnadsmassage, aromaterapimassage och inkluderar även inslag av Rungu-massage. Resultatet är ökad energinivå, återställd smidighet, lindrade muskelspänningar och minskad stress
+          </li>
+        </ul>
+        
+        
+        <img class="servicePicture" src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
       </div>
       
       <div class="contentRight">
         
       </div>
+    </div>
             
+
+
+/*Dont touch*/
 
         </div>
     )
