0c175264b89ee06896ae0a7f81fd419d0338c05f
Gele <gellehussen@gmail.com>
2023-11-26 22:13:04 +0100

    formatted treatment page

--- src/Components/HomePage/Treatmeants/Treatmeants.jsx
+++ src/Components/HomePage/Treatmeants/Treatmeants.jsx
@@ -1,55 +1,55 @@
 import "./style.css";
 
 
-const Treatments = ()=>{
+const Treatments = () => {
 
-    return(
+    return (
         <div className="treatmentContainer">
             <h3> Elementen och energin  Energi i rörelse och glädje</h3>
             <p> Efter många års studier av massage och naturliga läkningsmetoder, har vi omfamnat den unika och kraftfulla formen av afrikansk klassisk massage.</p>
-            
+
             <div className="treatmeantShowcase">
-            <img src="https://images.unsplash.com/photo-1540555700478-4be289fbecef?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                        <img src="https://images.unsplash.com/photo-1489659639091-8b687bc4386e?q=80&w=2673&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                        <img src="https://images.unsplash.com/photo-1496661415325-ef852f9e8e7c?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                        <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>            
-                        <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                        <img src="https://images.unsplash.com/photo-1496661415325-ef852f9e8e7c?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                
+                <img src="https://images.unsplash.com/photo-1540555700478-4be289fbecef?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                <img src="https://images.unsplash.com/photo-1489659639091-8b687bc4386e?q=80&w=2673&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                <img src="https://images.unsplash.com/photo-1496661415325-ef852f9e8e7c?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                <img src="https://images.unsplash.com/photo-1496661415325-ef852f9e8e7c?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
             </div>
 
 
             <div className="treatMeantDescription">
-           
-            <div className="treatMeantDescriptionCol">
-            <img src="https://images.unsplash.com/photo-1496661415325-ef852f9e8e7c?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
 
-           <div>  <h3> Butik Lojalitet Vi har högt fokus på</h3>
-<p> produktkvalitet, så alla produkter testas innan de säljs</p>
+                <div className="treatMeantDescriptionCol">
+                    <img src="https://images.unsplash.com/photo-1496661415325-ef852f9e8e7c?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
-</div>
-            </div>
+                    <div>  <h3> Butik Lojalitet Vi har högt fokus på</h3>
+                        <p> produktkvalitet, så alla produkter testas innan de säljs</p>
 
-
-            <div className="treatMeantDescriptionCol">
-            <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-            
-            <div> <h3>Naturliga Produkter Vi använder våra egna </h3>
-<p> utlandska produkter som är noggrant utvalda</p>
-
-</div>
-            </div>
+                    </div>
+                </div>
 
 
-            <div className="treatMeantDescriptionCol">
-            <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-          
-            <div className="descripionDetail">  <h3> Produkter</h3>
-           <p>  Vi erbjuder handel av våra premium produkter som används i tjänsterna</p>
+                <div className="treatMeantDescriptionCol">
+                    <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
-           </div>
+                    <div> <h3>Naturliga Produkter Vi använder våra egna </h3>
+                        <p> utlandska produkter som är noggrant utvalda</p>
 
-            </div>
+                    </div>
+                </div>
+
+
+                <div className="treatMeantDescriptionCol">
+                    <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+                    <div className="descripionDetail">  <h3> Produkter</h3>
+                        <p>  Vi erbjuder handel av våra premium produkter som används i tjänsterna</p>
+
+                    </div>
+
+                </div>
 
             </div>
 
