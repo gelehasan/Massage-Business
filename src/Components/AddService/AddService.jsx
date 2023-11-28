a6bf48756312f94530ad19e0b4cf4aac8b6f2257
Gele <gellehussen@gmail.com>
2023-11-28 22:09:25 +0100

    added form inputs for service content

--- src/Components/AddService/AddService.jsx
+++ src/Components/AddService/AddService.jsx
@@ -10,6 +10,14 @@ const AddService = ()=>{
             <label>Title</label>
             <input id="title" name="title"/>
 
+            <label>information om servicen</label>
+            <label> Paragrph 1</label>
+            <input type="text" name="info1" />
+
+            <label> Paragrph 2</label>
+            <input type="text" name="info2" />
+
+
             </form>
         </div>
     )
