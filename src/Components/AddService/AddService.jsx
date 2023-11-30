615fea53b8e8313131416e018242d255222d2fee
Gele <gellehussen@gmail.com>
2023-11-30 21:44:03 +0100

    Added ability to keep track of the code

--- src/Components/AddService/AddService.jsx
+++ src/Components/AddService/AddService.jsx
@@ -2,115 +2,96 @@ import React, { useState } from "react";
 import "./style.css";
 
 const AddService = () => {
-  const [listor, setListor] = useState([]);
-  const [listCount, setListCount] = useState(0);
+  const [formData, setFormData] = useState({
+    serviceTitle: "",
+    rubrik: "",
+    info1: "",
+    info2: "",
+    specialOmServicen: "",
+    listor: [],
+    imageOfService: "",
+    ovrigtInfomationTitle: "",
+    ovrigtInfomation: "",
+    otherList: [],
+    serviceImage: "",
+  });
 
-  const [otherList, setOtherList] = useState([]);
-  const [otherListCount, setOtherListCount] = useState(0);
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const handleAddLista = () => {
-    setListor([...listor, { id: listCount, value: "" }]);
-    setListCount(listCount + 1);
+    setFormData({
+      ...formData,
+      listor: [...formData.listor, { id: formData.listor.length, value: "" }],
+    });
   };
 
   const handleAddOtherList = () => {
-    setOtherList([...otherList, { id: otherListCount, value: "" }]);
-    setOtherListCount(otherListCount + 1);
+    setFormData({
+      ...formData,
+      otherList: [...formData.otherList, { id: formData.otherList.length, value: "" }],
+    });
   };
 
   const handleListaChange = (id, value) => {
-    const updatedListor = listor.map((item) =>
+    const updatedListor = formData.listor.map((item) =>
       item.id === id ? { ...item, value } : item
     );
-    setListor(updatedListor);
+    setFormData({
+      ...formData,
+      listor: updatedListor,
+    });
   };
 
   const handleOtherListChange = (id, value) => {
-    const updatedOtherList = otherList.map((item) =>
+    const updatedOtherList = formData.otherList.map((item) =>
       item.id === id ? { ...item, value } : item
     );
-    setOtherList(updatedOtherList);
+    setFormData({
+      ...formData,
+      otherList: updatedOtherList,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+ 
+    console.log(formData);
   };
 
   return (
     <div className="form-container">
-      <form className="service-form">
+      <form className="service-form" onSubmit={handleSubmit}>
+      
+
         <label>Service Title</label> <br />
-        <input id="title" name="serviceTitle" />
-        <br />
-
-        <label>Rubrik</label>
-        <br/>
-        <input type="text" name="rubrik" />
-       
-        <br />
-        <label>Paragrph 1</label>
-        <br />
-        <input type="text" name="info1" />
-        <br />
-
-        <label>Paragrph 2</label>
-        <br />
-        <input type="text" name="info2" />
-        <br />
-
-        <label>Vad är special om servicen</label>
-        <br />
-        <input type="text" name="specialOmServicen" />
+        <input
+          id="title"
+          name="serviceTitle"
+          value={formData.serviceTitle}
+          onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+        />
         <br />
 
         <label>Listor</label> <br />
-        <button type="button" onClick={handleAddLista}>
-        Lägg listor för vad är special om servicen
-        </button>
-        {listor.map((item) => (
-          <div key={item.id}>
-            <input
-              type="text"
-              value={item.value} 
-              onChange={(e) => handleListaChange(item.id, e.target.value)}
-            />
-          </div>
-        ))}
+{formData.listor.map((item) => (
+  <div key={item.id}>
+    <input
+      value={item.value}
+      onChange={(e) => handleListaChange(item.id, e.target.value)}
+    />
+  </div>
+))}
+<button type="button" onClick={handleAddLista}>
+  Add List Item
+</button>
+<br />
 
-        <label>Image of Service</label>
-        <br />
-        <input
-          type="text"
-          name="imageOfService"
-          placeholder="Image of Service"
-        />
-    
-       
-       <label>övrigt information title</label> <br />
-        <input type="text" name="ovrigtInfomationTitle"/>
-
-        <label>övrigt information</label> <br />
-        <input type="text" name="ovrigtInfomation"/>
-        <button type="button" onClick={handleAddOtherList}>
-          Lägg listor för övrigt information
-        </button>
-        {otherList.map((item) => (
-          <div key={item.id}>
-            <input
-              type="text"
-              value={item.value}
-              onChange={(e) => handleOtherListChange(item.id, e.target.value)}
-            />
-            <br />
-            
-          </div>
-        ))}
-
-<label>Bild på tjänsten</label> <br />
-            <input
-              type="text"
-              name="serviceImage"
-              placeholder="Service Image"
-            />
-            <br />
-
-            <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
