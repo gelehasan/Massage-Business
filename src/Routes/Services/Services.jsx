73f6eafa5b26f32b331fd193d31d940fc781646a
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-12-06 21:37:20 +0100

    Deleted file

--- src/Routes/Services/Services.jsx
+++ src/Routes/Services/Services.jsx
@@ -1,13 +1,1 @@
 
-import { useSelector } from "react-redux"
-
-const Services = ()=>{
-    const isServiceLoading = useSelector((state)=>state.services.isServiceLoading);
-    
-    return(
-
-        <div>
-
-        </div>
-    )
-}
