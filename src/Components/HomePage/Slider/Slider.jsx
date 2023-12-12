fb0ef13d5bf58a1f0ddb0d62fd88c3fbbfb38b60
Gele <gellehussen@gmail.com>
2023-12-12 22:01:53 +0100

    Added more sliders data

--- src/Components/HomePage/Slider/Slider.jsx
+++ src/Components/HomePage/Slider/Slider.jsx
@@ -6,26 +6,33 @@ const slides = [
   {
     h1Text: "Kärnan av Vår Existens",
     pText: "Förstå vikten Ubuntu i vår filosofi. Lär dig hur detta begrepp präglar vårt sätt att Se på sammanhang enhet och vårt ömsesidiga beroende.",
-    imgSrc: "noWay"
+    imgSrc: "https://res.cloudinary.com/ddeif6hmk/image/upload/v1702332335/main_gqbtfk.avif"
   },
   {
     h1Text: " Massage & Cosmetics",
    
     pText: "Upptäck vårt djupa engagemang för att förena kropp, själ och sinne genom afrikansk massage och filosin av Ubuntu",
-    imgSrc: "noWay"
+    imgSrc: "https://res.cloudinary.com/ddeif6hmk/image/upload/v1702332335/main_gqbtfk.avif"
   },
+  {
+    h1Text: "Harmoni med naturen",
+   
+    pText: "Utforska vårt engagemang för att leva i harmoni med den naturliga världen upptäckt hur vi integrerar Hållbarhet och miljömedvetenhet i vårt vardag.",
+    imgSrc: "https://res.cloudinary.com/ddeif6hmk/image/upload/v1702332335/main_gqbtfk.avif"
+  },
+  
 ];
 
 const Slider = () => {
   const [count, setCount] = useState(0);
   const [currentSlide, setCurrentSlide] = useState(slides[count]);
 
-  const { h1Text, pText } = currentSlide;
+  const { h1Text, pText, imgSrc } = currentSlide;
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCount((prevCount) => (prevCount + 1) % slides.length);
-      setCurrentSlide(slides[count]); // Issue: should be setCount((prevCount) => (prevCount + 1) % slides.length);
+      setCurrentSlide(slides[count]); 
     }, 3000);
 
     return () => clearInterval(interval);
@@ -40,7 +47,7 @@ const Slider = () => {
     <div className="slider-container">
       <img
         className="sliderImg"
-        src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702332335/main_gqbtfk.avif"
+        src={imgSrc}
         alt="Slider Background"
       />
       <div className="slides">
