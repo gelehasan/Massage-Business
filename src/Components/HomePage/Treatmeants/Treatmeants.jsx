8ac85304c02029098d508b7b6d4c267498d650de
Gele <gellehussen@gmail.com>
2023-12-13 22:54:02 +0100

    changed treatment pictures

--- src/Components/HomePage/Treatmeants/Treatmeants.jsx
+++ src/Components/HomePage/Treatmeants/Treatmeants.jsx
@@ -9,12 +9,12 @@ const Treatments = () => {
             <p> Efter många års studier av massage och naturliga läkningsmetoder, har vi omfamnat den unika och kraftfulla formen av afrikansk klassisk massage.</p>
 
             <div className="treatmeantShowcase">
-                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702415748/mass_ewqimn.avif" />
-                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702416019/sht_tn4qx7.jpg" />
-                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702415735/massage_safegn.avif" />
-                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702415735/mass_ghlbri.webp" />
-                <img src="https://images.unsplash.com/photo-1598901986949-f593ff2a31a6?q=80&w=2697&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702415737/kllk_ewvkq8.avif" />
+                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702504032/sh1_xqkfcz.jpg" />
+                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702504033/show1_l37ttp.jpg" />
+                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702504032/sh2_s0tfxt.jpg" />
+                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702504033/sh3_tmi4tu.jpg" />
+                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702504033/sh5_gya7ov.jpg" />
+                <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702504032/sh4_ypcnbw.webp" />
 
             </div>
 
