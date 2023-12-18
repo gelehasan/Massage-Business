4ae72cb460bc83ec2587dcd9596fd1d2b138f07a
Gele <gellehussen@gmail.com>
2023-12-18 20:52:28 +0100

    Fixed logo redirection to main page

--- src/Routes/Navbar/Nav.jsx
+++ src/Routes/Navbar/Nav.jsx
@@ -41,28 +41,21 @@ const Navbar = ()=>{
             
         <div class="navLogo">
         
-     <Link to={"/"}>   <div class="logoImage">
+     <Link to={"/"} className="Nav-link">   <div class="logoImage">
           <img src="https://res.cloudinary.com/ddeif6hmk/image/upload/v1702816901/RealLog_enhanced_dgoxtq.png"/>
       </div>
       </Link> 
+      <Link to={"/"} className="Nav-link">
       <div class="logoDescription">
         <p> AFROROOTS OF SWEDEN</p>
         <p class="bottomLogoText"> MASSAGE & COSMETIC</p>
       </div>
-      
+      </Link> 
       <img src={isMenuOn ?  XIcon : MenuIcon} onClick={toggleMenu} 
                     className="Menus"
                 />
     </div>
     
-            
-       {/*     <div className="navLogo">
-
-                
-              <Link to={"/"} >  <img src={MassageIcon} className="Logo "/>
-              </Link> 
-             
-    </div>*/}
 
         
 
