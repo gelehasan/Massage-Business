d983b97eb68a9d8af0c0b76ffa5ab6f258c142cc
Gele <gellehussen@gmail.com>
2023-12-18 21:01:04 +0100

    Gave showcase slider an animation

--- src/Components/HomePage/ServiceShowCase/ShowCase.jsx
+++ src/Components/HomePage/ServiceShowCase/ShowCase.jsx
@@ -1,9 +1,28 @@
 import "./style.css"
-
+import { useState, useEffect } from "react";
 const ShowCase = ()=>{
+    const [isVisible, setIsVisible] = useState(false);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const showCase = document.querySelector('.showCaseServiceContent');
+            const topPosition = showCase.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            // Check if the element is within the viewport
+            if (topPosition < windowHeight) {
+                setIsVisible(true);
+            }
+        };
+
+        // Attach the event listener
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup the event listener
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
     return(
-        <div className="showCaseServiceContent">
+        <div className={`showCaseServiceContent ${isVisible ? 'visible' : ''}`} >
         <div className="showCaseServiceLeft">
             <h1>Upptäck vårt breda
             utbud av skräddarsydda
