7ba9a2c1da8d6680dc83fa750e1163c224a24f04
Gele <gellehussen@gmail.com>
2024-01-28 21:08:52 +0100

    imrproved under consturvtion text

---
+++ src/underconstraction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UnderConstruction.css'; 
+const UnderConstruction = () => {
+    return (
+        <div className="under-construction">
+            <h1>Hemsidan under underhåll </h1>
+            <p>Vi arbetar  för att förbättra vår hemsida och vi är redo att lansera den snart!</p>
+        </div>
+    );
+};
+
+export default UnderConstruction;
