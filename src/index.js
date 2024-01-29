af22507be3b50db91a57fa11206fd9468b904697
Gele <gellehussen@gmail.com>
2024-01-29 20:59:02 +0100

    published website

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ import UnderConstruction from './underconstraction';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
     <Provider store={Store}>
     <BrowserRouter> 
-   <UnderConstruction />
+   < App/>
     </BrowserRouter>
     </Provider>
   </React.StrictMode>
